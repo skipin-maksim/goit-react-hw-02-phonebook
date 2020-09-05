5d018735a4ae62f55154ec786cd5c17ccb30abd4
Maksim Skipin <skipysmaks@gmail.com>
2020-09-05 21:42:51 +0300

    Updates

---
+++ precache-manifest.36de6a1b7c4d245ed761a78477494606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e273d7b6fda9f7cb6f0eee71fd768ffe",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "a17e2a8e59284bacc132",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.cc0c630c.chunk.css"
+  },
+  {
+    "revision": "8cb2133a36cace1911ed",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.356a6385.chunk.css"
+  },
+  {
+    "revision": "a17e2a8e59284bacc132",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0df2adff.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0df2adff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb2133a36cace1911ed",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.7511097a.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/goit-react-hw-02-phonebook/precache-manifest.36de6a1b7c4d245ed761a78477494606.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-phonebook/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
