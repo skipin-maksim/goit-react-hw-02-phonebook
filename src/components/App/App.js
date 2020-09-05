78c334cdc34a51f88a0e7ff51127f7bfb7c50101
Maksim Skipin <skipysmaks@gmail.com>
2020-09-05 13:26:10 +0300

    start work at task

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return <div>Gi</div>;
+  }
+}
+
+export default App;
